0d6a05870bd83851fa3de19f3a6df75229f7d97d
Arish <arishofficiall@gmail.com>
2024-07-19 08:33:50 +0530

    basic work

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+recipes = []
+
+@api_view(['GET'])
+def get_recipes(request):
+    return Response(recipes)
+
+@api_view(['POST'])
+def add_recipe(request):
+    recipe = request.data
+    recipes.append(recipe)
+    return Response(recipe, status=201)
